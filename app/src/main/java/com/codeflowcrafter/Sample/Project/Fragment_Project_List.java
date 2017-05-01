79816ac2161c6d0fa183f7f71006e9e2c224ecd6
Enrico <enrico_velasco@hotmail.com>
2017-05-01 13:06:53 +0800

    --amend

--- app/src/main/java/com/codeflowcrafter/Sample/Project/Fragment_Project_List.java
+++ app/src/main/java/com/codeflowcrafter/Sample/Project/Fragment_Project_List.java
@@ -6,6 +6,6 @@ import android.app.ListFragment;
  * Created by aiko on 5/1/17.
  */
 
-public class ProjectListActivity_Fragment_List extends ListFragment {
+public class Fragment_Project_List extends ListFragment {
 }
 
